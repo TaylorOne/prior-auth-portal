65b764c1f8ae83298615f4596d47a50791e776c8
Jordon Byers <jordonbyers@gmail.com>
2026-04-04 12:04:40 -0400

    Add getPatients and getServiceCodes endpoints in FE and corresponding types

--- PriorAuthWeb/src/api/PriorAuthApi.ts
+++ PriorAuthWeb/src/api/PriorAuthApi.ts
@@ -1,4 +1,6 @@
 import type { PriorAuthSummary } from "../types/PriorAuth";
+import type { Patient } from "../types/Patient";
+import type { ServiceCode } from "@/types/ServiceCode";
 
 const BASE_URL = import.meta.env.VITE_API_URL ?? "http://localhost:5054";
 
@@ -7,3 +9,16 @@ export async function getPriorAuthRequests(): Promise<PriorAuthSummary[]> {
   if (!response.ok) throw new Error("Failed to fetch prior auth requests");
   return response.json();
 }
+
+export async function getPatients(): Promise<Patient[]> {
+  const response = await fetch(`${BASE_URL}/patients`);
+  if (!response.ok) throw new Error("Failed to fetch patients");
+  return response.json();
+}
+
+export async function getServiceCodes(): Promise<ServiceCode[]> {
+  const response = await fetch(`${BASE_URL}/authrules/`);
+  if (!response.ok) throw new Error("Failed to fetch service codes");
+  return response.json();
+}
+
---
+++ PriorAuthWeb/src/types/ServiceCode.ts
@@ -0,0 +1,4 @@
+export interface ServiceCode {
+    code: string;
+    displayName: string;
+}
