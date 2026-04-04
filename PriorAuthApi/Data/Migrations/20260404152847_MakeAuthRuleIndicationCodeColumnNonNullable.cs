03ccaee9e483f3f40f4bf7f035e0e610fed9c331
Jordon Byers <jordonbyers@gmail.com>
2026-04-04 11:33:13 -0400

    Fix AuthRule IndicationCode nullability

---
+++ PriorAuthApi/Data/Migrations/20260404152847_MakeAuthRuleIndicationCodeColumnNonNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriorAuthApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeAuthRuleIndicationCodeColumnNonNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IndicationCode",
+                table: "AuthRules",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IndicationCode",
+                table: "AuthRules",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- PriorAuthApi/Entities/AuthRule.cs
+++ PriorAuthApi/Entities/AuthRule.cs
@@ -6,7 +6,7 @@ namespace PriorAuthApi.Entities
         public RequestType RequestType { get; set; }
         public required string CodeSystem { get; set; }  // e.g., "CPT", "HCPCS"
         public required string Code { get; set; }
-        public string? IndicationCode { get; set; }  // ICD-10 code for the indication
+        public required string IndicationCode { get; set; }  // ICD-10 code for the indication
         public required string DisplayName { get; set; }
         public string? IndicationDisplayName { get; set; }
         public required string FormDefinition { get; set; }       // JSON array
