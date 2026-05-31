c172f616b4443448d8dfa3d888321ae15d87810d
Jordon Byers <jordonbyers@gmail.com>
2026-05-31 16:44:05 -0400

    feat: add migration to remove default value for RequiresManualReview column

---
+++ PriorAuth.Data/Migrations/20260531203611_RemoveRequiresManualReviewDefault.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriorAuth.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRequiresManualReviewDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "RequiresManualReview",
+                table: "AuthRules",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "RequiresManualReview",
+                table: "AuthRules",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
