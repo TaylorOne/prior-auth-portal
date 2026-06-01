e716376da6d5bfc6a6859ba2bee779c4e0c7d5d7
Jordon Byers <jordonbyers@gmail.com>
2026-06-01 18:37:28 -0400

    fix: set minimum logging level to Information in Program.cs and update host.json structure

--- PriorAuthFunctions/Program.cs
+++ PriorAuthFunctions/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PriorAuth.Data;
 using PriorAuth.AuthEngine.Services;
+using Microsoft.Extensions.Logging;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -13,6 +14,8 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Logging.SetMinimumLevel(LogLevel.Information);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration["SqlConnectionString"]));
